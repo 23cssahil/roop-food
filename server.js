d5e67036173a73497515d4687ae51bb18d860341
23cssahil <23cssahil@gail.com>
2026-02-19 02:27:35 +0530

    Fix Express 5 catch-all syntax with named parameter

--- server.js
+++ server.js
@@ -206,7 +206,7 @@ app.put("/admin/order-done/:id", checkAdmin, (req, res) => {
 
 if (process.env.NODE_ENV === 'production' || true) {
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get('/*', (req, res) => {
+    app.get('/:path*', (req, res) => {
         res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
     });
 }
