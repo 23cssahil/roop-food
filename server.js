5bcee4670fbc4cbc2c9f95fa2f2b6ef3b62cd803
sahil khan <23cssahil@gmail.com>
2026-02-19 10:54:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -204,12 +204,13 @@ app.put("/admin/order-done/:id", checkAdmin, (req, res) => {
 
 // ================= PRODUCTION SERVING =================
 
-if (process.env.NODE_ENV === 'production' || true) {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.use((req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
     });
 }
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
